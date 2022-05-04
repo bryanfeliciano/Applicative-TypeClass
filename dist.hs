c182c4fdd8bbce7007b931332197ff879eb661e7
bryan <bryanfeliciano892@gmail.com>
2022-05-04 13:38:22 -0500

    Q28.1 and Q.28.2

--- dist.hs
+++ dist.hs
@@ -35,6 +35,21 @@ haversineMaybe Nothing _ = Nothing
 haversineMaybe _ Nothing = Nothing
 haversineMaybe (Just val1) (Just val2) = Just (haversine val1 val2)
 
+-- Q28.1 Writing haversineMaybe (listing 28.4) was straightforward. Write the function
+-- haversineIO without using <*>. Here’s the type signature:
+
+-- haversineIO :: IO LatLong -> IO LatLong -> IO Double
+-- haversineIO ioVal1 ioVal2 = do
+--     val1 <- ioVal1
+--     val2 <- ioVal2
+--     let dist = haversine val1 val2
+--     return dist
+
+-- Q28.2 Rewrite haversineIO using <*>
+
+haversineIO :: IO LatLong -> IO LatLong -> IO Double
+haversineIO ioVal1 ioVal2 = haversine <$> ioVal1 <*> ioVal2
+
 -- Printing your result -- 
 
 printDistance :: Maybe Double -> IO()
